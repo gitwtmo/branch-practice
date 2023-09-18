ca1f1340a40971d59f0175c866548381f83e6080
W-T-M-O <wtmo_dev@naver.com>
2023-09-18 12:17:21 +0900

    merge branch 'buzz'
    fixed conflict statement each fizz or buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,11 @@
 def fizzbuzz():
     for i in range(1,15+1):
-<<<<<<< HEAD
-        if i% == 3 ==0:
+        if i% 3 == 0:
             print(f'{i}th num is fizz')
-        else:
-            print('{i}th num')
-=======
-        if i%5==0:
+        elif i%5 == 0:
             print(f'{i}th num is buzz')
         else:
             print(f'{i}th')
->>>>>>> buzz
 
-if __init__ == '__main__':
+if __name__ == '__main__':
     fizzbuzz()
