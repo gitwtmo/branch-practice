af8d0a09379bb95dc572b77bdc1539571158840d
W-T-M-O <wtmo_dev@naver.com>
2023-09-18 12:11:16 +0900

    feat: add print fizz condition
    print fizz when num %3 == 0

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,9 @@
 def fizzbuzz():
-    print('hello')
+    for i in range(1,15+1):
+        if i% == 3 ==0:
+            print(f'{i}th num is fizz')
+        else:
+            print('{i}th num')
 
 if __init__ == '__main__':
     fizzbuzz()
